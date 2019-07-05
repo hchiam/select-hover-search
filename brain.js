78ec3650da59c13c5edb6ceb17936b2804b4254d
hchiam <hchiam@users.noreply.github.com>
2019-07-04 22:22:29 -0400

    set up. working.

--- brain.js
+++ brain.js
@@ -1,31 +1,188 @@
-let var1 = 1;
-let var2 = '2';
+let isSearching = false;
 
-chrome.storage.local.get('var1', function getData(data) {
-  let testButton = document.getElementById('test-button');
-  testButton.innerHTML = data.var1;
+document.addEventListener("mouseup", function(event) {
+  let selectedText = String(window.getSelection());
+  if (!selectedText.trim()) {
+    return;
+  }
+  if (selectedText.length > 100) {
+    alert('That text looks too long.');
+    return;
+  }
+  let mouseQuadrant = getMouseQuadrant(event);
+  let mouseCoordinates = getMouseCoordinates(event);
+  showIcons(selectedText, mouseQuadrant, mouseCoordinates);
 });
 
-chrome.storage.local.get('var2', function getData(data) {
-  let testButton2 = document.getElementById('test-button-2');
-  testButton2.innerHTML = data.var2;
-  for (let x = 0; x < 3; x++) {
-    let button = document.createElement("button");
-    button.innerHTML = x;
-    button.id = x;
-    button.addEventListener("click", function useSettings() {
-      var2 += 1;
-      chrome.storage.local.set({'var2': var2});
-      document.getElementById('output').innerHTML = var2;
-    });
-    var addButtonsHere = document.getElementById("add-buttons-here");
-    addButtonsHere.appendChild(button);
+function getMouseQuadrant(event) {
+  const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  let left = event.clientX;
+  let top = event.clientY;
+  if (left <= width/2 && top <= height/2) {
+    return 1;
+  } else if (left <= width/2 && top > height/2) {
+    return 3;
+  } else if (left > width/2 && top <= height/2) {
+    return 2;
+  } else if (left > width/2 && top > height/2) {
+    return 4;
   }
-});
+}
 
-chrome.storage.local.set(
-  {
-    'var1': var1,
-    'var2' : var2
+function getMouseCoordinates(event) {
+  let left = event.clientX;
+  let top = event.clientY;
+  return [left, top];
+}
+
+// show icons
+// for 1 second
+// in quadrants the mouse is not in
+function showIcons(selectedText, mouseQuadrant, mouseCoordinates) {
+  removeQuadrantButtons();
+  createBackground();
+  let searchEngineList = [
+    {
+      'searchEngine': 'Google',
+      'urlStart': 'https://www.google.com/search?q='
+    },
+    {
+      'searchEngine': 'Wiktionary',
+      'urlStart': 'https://en.wiktionary.org/wiki/'
+    }
+  ];
+  let searchEngineIndex = 0;
+  let quadrantsToUse = [];
+  let quadrantsToUseIndex = 0;
+  switch(mouseQuadrant) {
+    case 1:
+      quadrantsToUse = [5,7,8];
+      break;
+    case 2:
+      quadrantsToUse = [4,6,7];
+      break;
+    case 3:
+      quadrantsToUse = [2,3,5];
+      break;
+    case 4:
+      quadrantsToUse = [1,2,4];
+      break;
   }
-);
+  for (let i = 1; i <= 4 && searchEngineIndex < searchEngineList.length; i++) {
+    let quadrantToUse = quadrantsToUse[quadrantsToUseIndex];
+    let searchEngine = searchEngineList[searchEngineIndex].searchEngine;
+    let urlStart = searchEngineList[searchEngineIndex].urlStart;
+    createQuadrantButton(quadrantToUse, mouseCoordinates, searchEngine + ': <br/><br/>', selectedText, urlStart);
+    searchEngineIndex++;
+    quadrantsToUseIndex++;
+  }
+  setTimeout(function() {
+    removeQuadrantButtons();
+  }, 500);
+}
+
+function removeQuadrantButtons() {
+  var quadrantButtons = document.getElementsByClassName('select-hover-search-quadrants');
+  while (quadrantButtons.length>0) {
+      quadrantButtons[0].parentNode.removeChild(quadrantButtons[0]);
+  }
+}
+
+function createBackground() {
+  let div = document.createElement('div');
+  div.className = 'select-hover-search-quadrants';
+  div.style.cssText = `
+    all: initial;
+    z-index: 9998;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.25);
+  `;
+  document.body.appendChild(div);
+}
+
+function createQuadrantButton(quadrantToUse, mouseCoordinates, buttonText, selectedText, urlStart) {
+  let button = document.createElement('button');
+  button.id = 'select-hover-search-quadrant-' + quadrantToUse;
+  button.className = 'select-hover-search-quadrants';
+  button.innerHTML = buttonText + '<span style="font-size: medium;">' + selectedText + '</span>';
+  let left = 0;
+  let top = 0;
+  let distanceFromMouse = 150;
+  let widthOfButton = 100;
+  let relativeDistances = getRelativePosition(quadrantToUse, mouseCoordinates, distanceFromMouse, widthOfButton);
+  left = relativeDistances[0];
+  top = relativeDistances[1];
+  button.style.cssText = `
+    all: initial;
+    z-index: 9999;
+    position: fixed;
+    left: ${left}px;
+    top: ${top}px;
+    width: ${widthOfButton}px;
+    height: ${widthOfButton}px;
+    border-radius: 20px;
+    text-align: center;
+    background: rgba(255, 255, 255, 0.75);
+    font-family: avenir, arial;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  `;
+  button.onmouseover = function() {
+    button.style.cssText += 'background: rgb(138, 209, 172);';
+    if (!isSearching) {
+      setTimeout(function() {
+        let popup = window.open(urlStart + selectedText);
+        $(popup.document).load(function() {
+            isSearching = false;
+        });
+      }, 100);
+    }
+  };
+  document.body.appendChild(button);
+}
+
+function getRelativePosition(quadrantToUse, mouseCoordinates, distanceFromMouse, widthOfButton) {
+  let left = 0;
+  let top = 0;
+  switch(quadrantToUse) {
+    case 1:
+      left = mouseCoordinates[0] - distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] - distanceFromMouse - widthOfButton/2;
+      break;
+    case 2:
+      left = mouseCoordinates[0] - widthOfButton/2;
+      top = mouseCoordinates[1] - distanceFromMouse - widthOfButton/2;
+      break;
+    case 3:
+      left = mouseCoordinates[0] + distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] - distanceFromMouse - widthOfButton/2;
+      break;
+    case 4:
+      left = mouseCoordinates[0] - distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] - widthOfButton/2;
+      break;
+    case 5:
+      left = mouseCoordinates[0] + distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] - widthOfButton/2;
+      break;
+    case 6:
+      left = mouseCoordinates[0] - distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] + distanceFromMouse - widthOfButton/2;
+      break;
+    case 7:
+      left = mouseCoordinates[0] - widthOfButton/2;
+      top = mouseCoordinates[1] + distanceFromMouse - widthOfButton/2;
+      break;
+    case 8:
+      left = mouseCoordinates[0] + distanceFromMouse - widthOfButton/2;
+      top = mouseCoordinates[1] + distanceFromMouse - widthOfButton/2;
+      break;
+  }
+  return [left, top];
+}
